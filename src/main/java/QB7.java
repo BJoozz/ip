571dee778f0581e89c5d99164ce64ed5ea27fd97
BJoozz <123baoek31@gmail.com>
2025-08-22 14:35:52 +0700

    level-2: add tasks and list

--- src/main/java/QB7.java
+++ src/main/java/QB7.java
@@ -11,18 +11,38 @@ public class QB7 {
     }
 
     public static void main(String[] args) {
+        String[] tasks = new String[100];
+        int size = 0;
+
         System.out.println(LINE);
         System.out.println(" Hello! I'm QB7");
         System.out.println(" What can I do for you?");
         System.out.println(LINE);
+
         Scanner sc = new Scanner(System.in);
+
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
             if (input.equals("bye")) {
                 printBlock("Bye. Hope to see you again soon!");
                 break;
             }
-            printBlock(input);
+
+            if (input.equals("list")) {
+                if (size == 0) {
+                    printBlock(); // just the lines if nothing yet (optional)
+                } else {
+                    System.out.println(LINE);
+                    for (int i = 0; i < size; i++) {
+                        System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println(LINE);
+                }
+            } else {
+                // add task (assume size <= 100 per spec)
+                tasks[size++] = input;
+                printBlock("added: " + input);
+            }
         }
     }
 }
