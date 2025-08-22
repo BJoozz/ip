4bbbfb04efe68b1e7e2360013411573c70d27d1f
BJoozz <123baoek31@gmail.com>
2025-08-22 13:37:02 +0700

    level-0: rename to QB7; add greeting and exit

---
+++ src/main/java/QB7.java
@@ -0,0 +1,13 @@
+
+public class QB7 {
+    private static final String LINE = "____________________________________________________________";
+    public static void main(String[] args) {
+        // Level-0: greet and exit immediately
+        System.out.println(LINE);
+        System.out.println(" Hello! I'm QB7");
+        System.out.println(" What can I do for you?");
+        System.out.println(LINE);
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+}
