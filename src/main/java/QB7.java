b2bef544be87668a9758e37efe111cf4aa01ee58
BJoozz <123baoek31@gmail.com>
2025-08-22 14:23:15 +0700

    level-1: echo input; exit on 'bye'

--- src/main/java/QB7.java
+++ src/main/java/QB7.java
@@ -1,13 +1,28 @@
+import java.util.Scanner;
 
 public class QB7 {
     private static final String LINE = "____________________________________________________________";
+    private static void printBlock(String... lines) {
+        System.out.println(LINE);
+        for (String s : lines) {
+            System.out.println(" " + s);
+        }
+        System.out.println(LINE);
+    }
+
     public static void main(String[] args) {
-        // Level-0: greet and exit immediately
         System.out.println(LINE);
         System.out.println(" Hello! I'm QB7");
         System.out.println(" What can I do for you?");
         System.out.println(LINE);
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                printBlock("Bye. Hope to see you again soon!");
+                break;
+            }
+            printBlock(input);
+        }
     }
 }
