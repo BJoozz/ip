76396414fec978683ea15cf98fdf68e3dd46e765
BJoozz <123baoek31@gmail.com>
2025-09-15 22:57:25 +0800

    A-JavaDoc: add JavaDoc to all public classes and methods

--- src/main/java/jack/error/EmptyDescriptionException.java
+++ src/main/java/jack/error/EmptyDescriptionException.java
@@ -1,7 +1,17 @@
 package jack.error;
 
+/**
+ * Thrown to indicate that a command was given without a required description.
+ * <p>
+ * For example, {@code "todo"} without any following text.
+ */
 public class EmptyDescriptionException extends JackException {
-    public EmptyDescriptionException(String what) {
-        super("The " + what + " description cannot be empty.");
+    /**
+     * Creates a new {@code EmptyDescriptionException}.
+     *
+     * @param detail name of the command or field missing a description
+     */
+    public EmptyDescriptionException(String detail) {
+        super("The " + detail + " description cannot be empty.");
     }
 }
